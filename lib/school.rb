c6a42b40a668d94f0c0529a7a49f0b711ceda085
cedricn8 <cedcn-9mail@yahoo.com>
2022-09-11 13:11:12 -0700

    commit tree a321dceae8c13e62cfaab72de9df3a548ef4f551

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+    
+    def add_student(student, grade)
+        if roster[grade] == nil 
+           roster[grade] = []
+        end
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.map do |grade, student_array|
+            roster[grade] = student_array.sort
+        end
+        roster
+    end
+
+    
+end
